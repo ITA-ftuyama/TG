087fab06362163d3b892188dc1cfe7025affc0ce
Felipe Tuyama <ftuyama@gmail.com>
2016-12-12 18:47:36 -0200

    Fading LED

--- app.py
+++ app.py
@@ -2,17 +2,28 @@ import serial
 import time
 import win32api
 ser = serial.Serial('COM3', 9600, timeout=0)
-# while 1:
-#     print 'reading'
-#     print ser.readline()
-#     time.sleep(1)
-for i in range(100):
-    x, y = win32api.GetCursorPos()
-    if x > 100:
-        print "up"
-        ser.write('H')
+height = win32api.GetSystemMetrics(1)
+
+
+def read_ser():
+    while 1:
+        print 'reading'
+        print ser.readline()
+        time.sleep(1)
+
+
+def send_pos(n):
+    for i in range(n):
+        x, y = win32api.GetCursorPos()
+        y_string = string_number(y)
+        print y_string,
+        ser.write(y_string)
         time.sleep(0.1)
-    else:
-        print "down"
-        ser.write('L')
-        time.sleep(0.1)
+    ser.write("x")
+
+
+def string_number(y):
+    y = (height - y) * 255 / height
+    return str(1000 + int(y))[-3:] + "\n"
+
+send_pos(100)
--- led/led.ino
+++ led/led.ino
@@ -1,25 +1,34 @@
 int incomingByte = 0;
 int ledPin = 11;
+String inString;
+int number;
  
 void setup(){
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
- 
-void loop(){
+
+void send_numbers(){
   // Send data
   // incomingByte += 1;
   // Serial.println(incomingByte);
   // delay(1000);
-
-  // Read data
-  incomingByte = Serial.read();
+}
  
-  if(incomingByte == 'H') {
-    digitalWrite(ledPin, HIGH);
-  } else if(incomingByte == 'L') {
-     digitalWrite(ledPin, LOW);
-  }
-  // analogWrite(ledPin, incomingByte);
-  delay(100);
+void loop(){
+
+    // Read data
+    int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      inString += (char)inChar;
+    } else if (inChar == 'x') {
+      analogWrite(ledPin, 0);
+    } else if (inChar == '\n') {
+      int number = inString.toInt();
+      number = number / 10;
+      analogWrite(ledPin, number);
+      inString = "";
+    }
+
+    delay(10);
 }
